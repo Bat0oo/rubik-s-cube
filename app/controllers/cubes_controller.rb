3a1ea8cf35e0ba70ff89d09514f02c27aaaf4c65
Bat0oo <miloradmaksic01@gmail.com>
2025-08-19 17:47:26 +0200

    Rotation controller

---
+++ app/controllers/cubes_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::CubesController <ApplicationController
+  before_action :set_cube
+
+def show
+  render json: {
+    status: 'succes',
+    data: @cube.to_json
+  }
+end
+
+def rotate
+  begin
+    service = CubeRotationService.new(@cube)
+    service.rotate(params[:move])
+
+    render json: {
+      status: 'succes',
+      message: "Cube rotated with move #{params[:move]}",
+      data: @cube.to_json
+    }
+  rescue ArgumentError => e
+    render json: {
+      status: 'error',
+      message: e.message,
+    }, status :bad_request
+  end
+end
+
+def reset
+  @cube.reset_to_solved
+  
+  render json: {
+    status: 'succes',
+    message: "Cube is solved",
+    data: @cube.to_json
+  }
+end
+
+private
+def set_cube
+  @cube=RubiksCube.current_cube
+end
+
+end
