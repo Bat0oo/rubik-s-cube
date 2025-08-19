bf69b4989d77551469ac5937698109de7641fc6b
Bat0oo <miloradmaksic01@gmail.com>
2025-08-19 15:17:16 +0200

    ci and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resource :cube, only: [:show] do
+        post :rotate
+        post :reset
+      end
+    end
+  end
+
+
+
 end
